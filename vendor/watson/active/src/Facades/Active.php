d2e48de0ac272fecf2e60d8443ac3048ab1d5a6b
SergeyOctavian <rserg.work@gmail.com>
2015-10-23 14:53:23 +0300

    delete node_modules and vendor

--- vendor/watson/active/src/Facades/Active.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Watson\Active\Facades;
-
-use Illuminate\Support\Facades\Facade;
-
-class Active extends Facade
-{
-	protected static function getFacadeAccessor() { return 'active'; }
-}
