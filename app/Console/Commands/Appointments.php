d1d7fe2941c5b74a2342bb61d73bf7b3aa64b4ab
tabuna <bliz48rus@gmail.com>
2015-12-16 10:11:36 +0300

    Убийца выгрузки, если опаздываю

--- app/Console/Commands/Appointments.php
+++ app/Console/Commands/Appointments.php
@@ -60,93 +60,96 @@ class Appointments extends Command
             }
         }
 
+        $this->appointment();
 
-        while (true) {
-
-
-            $files = collect(Storage::files(''));
-
-            $filtered = $files->filter(function ($item) {
-                return str_is('MiD_*-OUT.xml', $item);
-            });
+    }
 
 
-            if ($filtered->count() > 4) {
-                foreach ($filtered as $file1COUT) {
-                    Storage::delete($file1COUT);
-                }
-                $filtered = [];
-            }
+    protected function appointment()
+    {
+
+        $files = collect(Storage::files(''));
+
+        $filtered = $files->filter(function ($item) {
+            return str_is('MiD_*-OUT.xml', $item);
+        });
 
 
-            DB::connection()->disableQueryLog();
-
+        if ($filtered->count() > 4) {
             foreach ($filtered as $file1COUT) {
+                Storage::delete($file1COUT);
+            }
+            $filtered = [];
+        }
 
 
-                if (Storage::exists($file1COUT)) {
+        DB::connection()->disableQueryLog();
 
-                    $xml = simplexml_load_string(Storage::get($file1COUT));
-                    $date = date("Y-m-d G:i:s");
-                    $doctor = [];
+        foreach ($filtered as $file1COUT) {
 
-                    foreach ($xml->Record as $value) {
 
-                        $doctor[] = [
-                            'id' => $value->attributes()->ID,
+            if (Storage::exists($file1COUT)) {
+
+                $xml = simplexml_load_string(Storage::get($file1COUT));
+                $date = date("Y-m-d G:i:s");
+                $doctor = [];
+
+                foreach ($xml->Record as $value) {
+
+                    $doctor[] = [
+                        'id' => $value->attributes()->ID,
+                        'subdivision' => (string)$value->PODR,
+                        'specialization' => (string)$value->SPEC,
+                        'name' => $value->SOTR,
+                        'created_at' => $date,
+                        'updated_at' => $date,
+                    ];
+
+                    $entry = [];
+                    foreach ($value->ZAPIS->T as $zapis) {
+                        $entry[] = [
+                            'beginning' => (int)$zapis->attributes()->S,
+                            'end' => (int)$zapis->attributes()->PO,
+                            '1c_busy' => (int)$zapis->attributes()->BUSY,
+                            'timetable_id' => (string)$value->attributes()->ID,
+                            'upload' => true,
                             'subdivision' => (string)$value->PODR,
-                            'specialization' => (string)$value->SPEC,
-                            'name' => $value->SOTR,
-                            'created_at' => $date,
-                            'updated_at' => $date,
                         ];
-
-                        $entry = [];
-                        foreach ($value->ZAPIS->T as $zapis) {
-                            $entry[] = [
-                                'beginning' => (int)$zapis->attributes()->S,
-                                'end' => (int)$zapis->attributes()->PO,
-                                '1c_busy' => (int)$zapis->attributes()->BUSY,
-                                'timetable_id' => (string)$value->attributes()->ID,
-                                'upload' => true,
-                                'subdivision' => (string)$value->PODR,
-                            ];
-                        }
-
-                        DB::table('entry')->insert($entry);
-
-
                     }
 
-                    DB::table('timetable')->truncate();
-                    DB::table('timetable')->insert($doctor);
-
-                    DB::table('entry')->where('upload', 'false')->delete();
-                    DB::table('entry')->where('upload', true)->update([
-                        'upload' => false
-                    ]);
+                    DB::table('entry')->insert($entry);
 
 
-                    Storage::delete($file1COUT);
-                    $this->info('Команда выгрузки xml файла расписания отработала');
-
-                } else {
-                    $this->info('xml файл не существует');
                 }
 
+                DB::table('timetable')->truncate();
+                DB::table('timetable')->insert($doctor);
 
-                $Apportointments = Appoint::where('updated_at', '>', Carbon::now()->subWeeks(1))->get();
-                $Views = view('XML.appointments', [
-                    'Record' => $Apportointments
-                ])->render();
-                Storage::put(time() . '-export.xml', $Views);
+                DB::table('entry')->where('upload', 'false')->delete();
+                DB::table('entry')->where('upload', true)->update([
+                    'upload' => false
+                ]);
 
+
+                Storage::delete($file1COUT);
+                $this->info('Команда выгрузки xml файла расписания отработала');
+
+            } else {
+                $this->info('xml файл не существует');
             }
 
+
+            $Apportointments = Appoint::where('updated_at', '>', Carbon::now()->subWeeks(1))->get();
+            $Views = view('XML.appointments', [
+                'Record' => $Apportointments
+            ])->render();
+            Storage::put(time() . '-export.xml', $Views);
 
         }
 
+
     }
+
 
 
     /**
