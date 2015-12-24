36b50ac4175d9829878ef956526f426d2eca6bfb
tabuna <bliz48rus@gmail.com>
2015-12-24 12:25:01 +0300

    medicalsystem to zdorove

--- config/socket.php
+++ config/socket.php
@@ -3,19 +3,25 @@
 return [
 
     /*
-     * Url adress
-     *
-     * @var string
-     */
+   * $httpHost HTTP hostname clients intend to connect to.
+   * MUST match JS `new WebSocket('ws://$httpHost')
+   */
 
-    'url' => '193.111.141.198',
+    'httpHost' => '193.111.141.198',
 
 
     /*
-     * Socket Port
-     *
-     * @var number
+     * Port to listen on. If 80, assuming production,
+     * Flash on 843 otherwise expecting Flash to be proxied through 8843
      */
 
-    'port' => '8991'
+    'port' => '8991',
+
+
+    /*
+     *IP address to bind to. Default is localhost/proxy only.
+     *'0.0.0.0' for any machine.
+     */
+    'address' => '0.0.0.0'
+
 ];
