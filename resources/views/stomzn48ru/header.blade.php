6c4bea837caa0b144359c0d3f6e220de6240e158
mamau <mopckou_pak48rus@mail.ru>
2016-01-13 17:27:39 +0300

    Хедер+Слайдер

--- resources/views/stomzn48ru/header.blade.php
+++ resources/views/stomzn48ru/header.blade.php
@@ -4,145 +4,225 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex"/>
-    <title>WTF TITLE</title>
+    <title>Стоматология</title>
 
-    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/fonts/font.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/tooltipster.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/tooltipster-light.css">
     <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/animations.css">
-    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/styles.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/styles-mirror.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/settings.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/stm-icomoon.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/jquery-ui.min.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/jquery-ui.datepicker.min.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/jquery-ui.slider.min.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/jquery-ui-timepicker-addon.min.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/jquery.fancybox.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/select2.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/frontend_customizer.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/skin_2.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/skin_3.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/skin_4.css">
+    <link rel="stylesheet" type="text/css" href="/stomzn48.ru/theme/css/js_composer.css">
+
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/jquery1.11.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/jquery-migrate.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/spin.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/spin.jquery.js"></script>
+
+
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/functions.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/jquery.form.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/scripts.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/jquery.cookie.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/core.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/datepicker.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/widget.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/mouse.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/slider.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/jquery-ui-timepicker-addon.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/jquery.fancybox.pack.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/select2.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/custom.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/js_composer_front.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/slick.min.js"></script>
+    <script type="text/javascript" src="/stomzn48.ru/theme/js/countUp.min.js"></script>
+
 
 </head>
 <body>
-<div class="header">
-    <div class="header-bg"></div>
-    <div class="container">
-        <div class="links">
-            <img src="/stomzn48.ru/img/enter.png"><a href="" title="Вход в личный кабинет ">Вход</a>
-            <img src="/stomzn48.ru/img/reg.png"><a href="" title="Регистрация новго пользователя">Регистрация</a>
-        </div>
-        <div class="top-contacts">
+
+<header id="header">
+
+    <div class="mobile_search">
+        <form method="get" class="search-form" action="index.html">
+            <input type="search" placeholder="Search" value="" name="s">
+            <button type="submit" class="button"><i class="stm-icon-search"></i></button>
+        </form>
+    </div>
+
+    <div class="top_nav affix-top">
+        <div class="container">
             <div class="row">
-                <div class="col-md-4">
-                    <div class="company-description">
-                        <div>Лечение и протезирование зубов в липецке</div>
-                        <div class="social">
-                            <div class="row">
-                                <div class="col-md-7">Присоединяйтесь к нам в соцсетях:</div>
-                                <div class="col-md-5 social-links">
-                                    <a href="http://vk.com/zdorovie_nacii48" alt="Мы вконтакте" title="Мы вконтакте"></a>
-                                    <a href="http://www.odnoklassniki.ru/group52174602436776" alt="Мы в одноклассниках" title="Мы в одноклассниках"></a>
-                                    <a href="https://www.facebook.com/groups/zdorovie48/ " alt="Мы на фейсбуке" title="Мы на фейсбуке"></a>
-                                </div>
-                            </div>
+
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                    <div class="logo">
+                        <a href="/">
+                            <img src="/stomzn48.ru/img/logo_blue.png" alt="Dent-All"></a>
+                        <div id="menu_toggle">
+                            <button></button>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4 logo">
-                    <a href="/"><img alt="Здоровье нации <<Стоматология>>" title="Главная страница" src="/stomzn48.ru/img/logo.png"></a>
+
+                <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+                    <ul id="menu-primary-menu" class="top_nav_menu">
+                        <li id="menu-item-4" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-4">
+                            <a href="/">Главная</a>
+                        </li>
+                        <li id="menu-item-648" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-648">
+                            <a href="/">О компании</a>
+                            <ul class="sub-menu">
+                                <li id="menu-item-730" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-730">
+                                    <a href="/">Контакты</a>
+                                </li>
+                                <li id="menu-item-750" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-750">
+                                    <a href="/">О нас</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li id="menu-item-889" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-889">
+                            <a href="/">Технологии</a>
+                        </li>
+                        <li id="menu-item-770" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-770">
+                            <a href="/">Оборудование</a>
+                        </li>
+                        <li id="menu-item-608" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-608">
+                            <a href="/">Материалы</a>
+
+                        </li>
+                        <li id="menu-item-902" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-902">
+                            <a href="/">Каталог</a>
+                        </li>
+                        <li id="menu-item-902" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-902">
+                            <a href="/">Новости</a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="col-md-4 addres-top">
-                    <div>г. Липецк, ул.Советская, д. 66; ул.Фрунзе, д.14</div>
-                    <div><span>+7 (4742) </span>227-887</div>
-                    <div><button><span>ЗАКАЗАТЬ ЗВОНОК</span><span></span></button></div>
+
+            </div>
+        </div>
+        <div class="mobile_menu">
+            <ul id="menu-primary-menu-1" class="top_mobile_menu">
+                <li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-4">
+                    <a href="/">Главная</a>
+                </li>
+                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-648">
+                    <a href="/">О компании</a>
+                    <span class="arrow"><i class="fa fa-chevron-down"></i></span>
+                    <ul class="sub-menu">
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-730">
+                            <a href="/">Контакты</a>
+                        </li>
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-750">
+                            <a href="/">О нас</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-889">
+                    <a href="/">Технологии</a>
+                </li>
+                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-770">
+                    <a href="/">Оборудование</a>
+                </li>
+                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-608">
+                    <a href="/">Материалы</a>
+                </li>
+                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-902">
+                    <a href="/">Каталог</a>
+                </li>
+                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-902">
+                    <a href="/">Новости</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="top_info_boxes">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                    <div class="top_info_box">
+                        <div class="icon">
+                            <i class="stm-icon-roundels"></i>
+                        </div>
+                        <div class="text">
+                            <strong>Свяжитесь с нами +7 (4742) 227-887</strong>
+                            <span>г. Липецк, ул.Советская, д. 66; ул.Фрунзе, д.14</span>
+                        </div>
+                    </div>
                 </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                    <div class="top_info_box">
+                        <div class="icon">
+                            <i class="stm-icon-clock"></i>
+                        </div>
+                        <div class="text">
+                            <strong>Рабочий день</strong>
+                            <span>Пон - Пт: 8:00 - 5:00, Сб-Вс: 9:00 - 14:00</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 text-right">
+                    <div class="top_info_box linked">
+                        <div class="icon">
+                            <i class="stm-icon-calculator"></i>
+                        </div>
+                        <div class="text">
+                            <strong>Запишитесь на приём</strong>
+                            <span>Это просто</span>
+                        </div>
+                        <a href="/" class="link"></a>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
-    <div class="banner-menu">
-        <div class="container-fluid">
-            <!-- МЕНЮ -->
-            <div class="container menu-container">
-                <nav class="navbar navbar-default navbar-custom">
-                    <div class="navbar-double"></div>
-                    <div class="container-fluid">
-                        <!-- Brand and toggle get grouped for better mobile display -->
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
+    <div class="bs-example" data-example-id="simple-carousel">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 
-                        </div>
-
-                        <!-- Collect the nav links, forms, and other content for toggling -->
-                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                            <ul class="nav navbar-nav custom-nav">
-                                {!! Menu::getLi(10,'topmain','') !!}
-                                <!--<li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="#">Action</a></li>
-                                        <li><a href="#">Another action</a></li>
-                                        <li><a href="#">Something else here</a></li>
-                                        <li role="separator" class="divider"></li>
-                                        <li><a href="#">Separated link</a></li>
-                                        <li role="separator" class="divider"></li>
-                                        <li><a href="#">One more separated link</a></li>
-                                    </ul>
-                                </li>-->
-                            </ul>
-                            <form class="navbar-form navbar-right" role="search">
-                                <div class="form-group custom-loop">
-                                    <input type="text" class="form-control" placeholder="Поиск по сайту">
-                                    <button type="submit" class="btn btn-default"></button>
-                                </div>
-
-                            </form>
-                        </div><!-- /.navbar-collapse -->
-                    </div><!-- /.container-fluid -->
-                </nav>
-            </div><!-- МЕНЮ end -->
-            <div class="banner-carousel">
-
-                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                    </ol>
-
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner" role="listbox">
-                        <div class="item active">
-                            <img src="/stomzn48.ru/img/banner1.png" alt="...">
-                            <div class="carousel-caption container">
-                                <div>
-                                    Станьте обладателем белоснежной
-                                    Улыбки всего за два визита к
-                                    Стоматологу!
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Controls
-                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                    -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img  src="/stomzn48.ru/img/banner1.png">
                 </div>
-                <div class="form-consult container">
-                    <div class="form-zapisi">
-                        <form>
-                            <div class="form-group">
-                                <div>ЗАПИСАТЬСЯ НА КОНСУЛЬТАЦИЮ</div>
-                                <input required placeholder="Ваше имя:"><input required placeholder="Ваш телефон:"><input required placeholder="Ваш E-mail:">
-                                <textarea required placeholder="Подробнее:"></textarea>
-                                <div><button>ОТПРАВИТЬ</button></div>
-                            </div>
-                        </form>
-                    </div>
+                <div class="item">
+                    <img  src="/stomzn48.ru/img/banner1.png" >
                 </div>
+
+            </div>
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+            <div class="wrrr">
+
             </div>
         </div>
-    </div><!-- banner-menu end -->
-</div>
+    </div>
+</header>
 @yield('content')
 @include('stomzn48ru/footer')
