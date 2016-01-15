de38df3bf81bf44f352d69dbacc734751d67cc85
mamau <mopckou_pak48rus@mail.ru>
2016-01-15 10:00:33 +0300

    Картинка с текстом

--- resources/views/stomzn48ru/header.blade.php
+++ resources/views/stomzn48ru/header.blade.php
@@ -223,24 +223,19 @@
 
             <div class="carousel-inner" role="listbox">
                 <div class="item">
-                    <img  src="/stomzn48.ru/img/banner1.png">
-                </div>
-                <div class="item">
-                    <img  src="/stomzn48.ru/img/banner1.png" >
+                    <img  src="/stomzn48.ru/img/banner1.png" alt="Альт для картинки">
+                    <div class="carousel-caption">
+                        Прекрасное описание для картинки
+                    </div>
+                    <!--Это бекграунд у картинки слайдера-->
+                    <div class="wrrr"></div>
+                    <!--Это бекграунд у картинки слайдера-->
                 </div>
 
             </div>
-            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-            <div class="wrrr">
 
-            </div>
+
+
         </div>
         <script type="text/javascript">
             jQuery(document).ready(function(){
