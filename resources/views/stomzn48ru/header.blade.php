500a9e007df0e2cf268a0d88a593bbba5db4648c
mamau <mopckou_pak48rus@mail.ru>
2016-01-15 10:05:34 +0300

    Слайдер сделан

--- resources/views/stomzn48ru/header.blade.php
+++ resources/views/stomzn48ru/header.blade.php
@@ -231,10 +231,26 @@
                     <div class="wrrr"></div>
                     <!--Это бекграунд у картинки слайдера-->
                 </div>
-
+                <div class="item">
+                    <img  src="/stomzn48.ru/img/banner2.jpg" alt="Альт для картинки2">
+                    <div class="carousel-caption">
+                        Прекрасное описание для картинки 2
+                    </div>
+                    <!--Это бекграунд у картинки слайдера-->
+                    <div class="wrrr"></div>
+                    <!--Это бекграунд у картинки слайдера-->
+                </div>
             </div>
 
-
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
 
         </div>
         <script type="text/javascript">
