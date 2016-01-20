b0a880a103f9f83b26169d87227b3e6460834bc7
mamau <mopckou_pak48rus@mail.ru>
2016-01-20 17:23:27 +0300

    Немного верстки

--- resources/views/luchiki48ru/header.blade.php
+++ resources/views/luchiki48ru/header.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" type="text/css" href="/luchiki48.ru/theme/css/woocommerce-smallscreen.css">
     <link rel="stylesheet" href="/luchiki48.ru/theme/css/styles.css">
     <link rel="stylesheet" type="text/css" href="/luchiki48.ru/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
     <script src="/luchiki48.ru/theme/js/all.min.js"></script>
     <script src="/luchiki48.ru/theme/js/cloudflare.min.js"></script>
@@ -16,6 +17,35 @@
 
 </head>
 <body>
+    <!--Верхняя плашка с адресами-->
+    <div class="site-top site-top-4 header-4">
+        <div class="container">
+            <div class="site-top-left">
+
+                <div class="site-top-content">
+                    <ul>
+                        <li><i class="fa fa-phone-square"></i>+7 (4742) 77 03 97</li>
+                    </ul>
+                </div>
+                <div class="site-top-content">
+                    <i class="fa fa-home"></i>г. Липецк, ул. Неделина, 15-а
+                </div>
+                <div class="site-top-content">
+                    <i class="fa fa-at"></i>info@luchiki.ru
+                </div>
+
+            </div>
+            <div class="site-top-right">
+                <ul class="social-link">
+                    <li><a href="http://www.vk.com" target="_blank"><i class="fa fa-vk"></i></a></li>
+                    <li><a href="http://www.twitter.com" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="http://www.ok.ru" target="_blank"><i class="fa fa-odnoklassniki"></i></a></li>
+                    <li><a href="http://www.facebook.com" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!--Верхняя плашка с адресами-->
     <!--Шапка и меню-->
     <header class="header-4 ">
         <div class="header">
@@ -24,8 +54,9 @@
                     <div class="navbar-header">
                         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="sr-only">Toggle navigation</span> <span
                                     class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span></button>
-                        <div class="header_logo"><a href="../cupid.1.html" title="CUPID | Adorable Kindergarten - Just another WordPress site" rel="home">
-                                <div><img src="/luchiki48.ru/uploads/2015/03/logo-4.png" width="171" alt="CUPID | Adorable Kindergarten - Just another WordPress site"></div>
+                        <div class="header_logo">
+                            <a href="/" title="CUPID | Adorable Kindergarten - Just another WordPress site" rel="home">
+                                <div><img src="/luchiki48.ru/img/logo_childs3.png" alt="CUPID | Adorable Kindergarten - Just another WordPress site"></div>
                             </a></div>
                     </div>
                     <div class="icon-search-menu"><i class="fa fa-search"></i></div>
--- resources/views/luchiki48ru/index.blade.php
+++ resources/views/luchiki48ru/index.blade.php
@@ -1,33 +1,127 @@
 @extends('luchiki48ru/header')
 @section('content')
-    <!--Карусель на главной-->
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<div id="wrapper-content">
+    <main role="main" class="site-content">
+        <div id="post-189" class="post-189 page type-page status-publish hentry">
+            <div class="entry-content">
+                <!--Карусель на главной-->
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 
-        <div class="carousel-inner" role="listbox">
-            <div class="item active">
-                <img src="/luchiki48.ru/uploads/revslider/slider_02/img-slider-01_resize.png" alt="слайд1">
-                <div class="carousel-caption">
-                    Прекрасное описание для слайда
-                </div>
-            </div>
-            <div class="item">
-                <img src="/luchiki48.ru/uploads/revslider/slider_04/img-slider-04.png" alt="слайд2">
-                <div class="carousel-caption">
-                    Прекрасное описание для слайда 2
-                </div>
-            </div>
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="/luchiki48.ru/uploads/revslider/slider_02/img-slider-01_resize.png" alt="слайд1">
+                            <div class="carousel-caption">
+                                Прекрасное описание для слайда
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="/luchiki48.ru/uploads/revslider/slider_04/img-slider-04.png" alt="слайд2">
+                            <div class="carousel-caption">
+                                Прекрасное описание для слайда 2
+                            </div>
+                        </div>
 
+                    </div>
+
+                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+                <!--Карусель на главной-->
+
+                <!--Учиться легко и с удовольствием-->
+                <div class="container">
+                    <div class="vc_row wpb_row vc_row-fluid cupid-margin-bottom-115">
+                        <div class="wpb_column vc_column_container vc_col-sm-4">
+                            <div class="wpb_wrapper">
+                                <div class="wpb_single_image wpb_content_element vc_align_center">
+                                    <div class="wpb_wrapper">
+                                        <div class="vc_single_image-wrapper  vc_box_border_grey">
+                                            <img width="350" height="363" src="/luchiki48.ru/uploads/2015/02/bg-services.jpg" class="vc_single_image-img attachment-full" alt="bg-services">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cupid-padding-left-35 wpb_column vc_column_container vc_col-sm-8">
+                            <div class="wpb_wrapper">
+                                <div class="cupid-introduction left cupid-margin-bottom-35">
+                                    <h3>We have been <b>educating children for over fifteen years.</b>
+                                        Our goal is to <b>create a place that engages each child.</b>
+                                    </h3>
+
+                                    <p>
+                                        By learning about those of differing social, cultural and religious perspectives, young people build their awareness of the role of
+                                       faith in their lives and in the lives of others.
+                                    </p>
+                                </div>
+                                <div class="fullwidth">
+                                    <div class="vc_row wpb_row vc_inner vc_row-fluid">
+                                        <div class="wpb_column vc_column_container vc_col-sm-6">
+                                            <div class="wpb_wrapper">
+                                                <div id="icon-box-3019-5054" class="cupid-icon-box style2 left ">
+                                                    <a href="index.html%3Fp=189.html"><i class="fa fa-tags"></i></a>
+                                                    <h4>Activity Rooms</h4>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="wpb_column vc_column_container vc_col-sm-6">
+                                            <div class="wpb_wrapper">
+                                                <div id="icon-box-2878-8462" class="cupid-icon-box style2 left ">
+                                                    <a href="index.html%3Fp=189.html"><i class="fa fa-plane"></i></a>
+                                                    <h4>Varied Classes</h4>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="wpb_column vc_column_container vc_col-sm-6">
+                                            <div class="wpb_wrapper">
+                                                <div id="icon-box-9590-2325" class="cupid-icon-box style2 left ">
+                                                    <a href="index.html%3Fp=189.html"><i class="fa fa-thumb-tack"></i>
+                                                    </a><h4>Music And Art Room</h4>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="wpb_column vc_column_container vc_col-sm-6">
+                                            <div class="wpb_wrapper">
+                                                <div id="icon-box-4851-4352" class="cupid-icon-box style2 left ">
+                                                    <a href="index.html%3Fp=189.html"><i class="fa fa-gamepad"></i></a>
+                                                    <h4>Multimedia Centre</h4>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="wpb_column vc_column_container vc_col-sm-6">
+                                            <div class="wpb_wrapper">
+                                                <div id="icon-box-9456-7143" class="cupid-icon-box style2 left ">
+                                                    <a href="index.html%3Fp=189.html"><i class="fa fa-cube"></i>
+                                                    </a>
+                                                    <h4>Play Area</h4>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="wpb_column vc_column_container vc_col-sm-6">
+                                            <div class="wpb_wrapper">
+                                                <div id="icon-box-2417-4927" class="cupid-icon-box style2 left ">
+                                                    <a href="index.html%3Fp=189.html"><i class="fa fa-bullhorn"></i>
+                                                    </a>
+                                                    <h4>Full Day Sessions</h4>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--Учиться легко и с удовольствием-->
+            </div>
         </div>
+    </main>
+</div>
 
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-    <!--Карусель на главной-->
 @endsection
