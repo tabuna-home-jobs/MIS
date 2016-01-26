173ce07bb76114ea35ec32263ff7f6f8ad139dd6
tabuna <bliz48rus@gmail.com>
2016-01-26 10:47:16 +0300

    Личный кабинет

--- resources/views/zdorovie48ru/header.blade.php
+++ resources/views/zdorovie48ru/header.blade.php
@@ -132,7 +132,7 @@
                     </div>
                     <ul class="nav navbar-nav">
 
-
+                    <!--
                         <li style="padding-bottom: 5px;" role="presentation" class=" dropdown" >
                             <a id="onas" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">О нас</a>
                             <ul id="menu1" class="dropdown-menu" aria-labelledby="drop4">
@@ -140,6 +140,11 @@
                                 <li><a href="/page/55">Наши лицензии</a></li>
                             </ul>
                         </li>
+                        -->
+
+                        <li><a href="/page/18">Информация о нас</a></li>
+                        <li><a href="/page/55">Наши лицензии</a></li>
+
                         <li class="{{Active::route('appointment')}}"><a href="/appointment">Запись на приём</a></li>
                         <!--<li class="{{Active::route('login')}}"><a href="/login">Личный кабинет</a></li>-->
                        <!-- <li class="{{Active::route('medencyclopedia.*')}}"><a href="/medencyclopedia">Энциклопедия</a></li> -->
