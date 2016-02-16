6e06232b4b9a907057c2377c02342fb2e4660633
tabuna <bliz48rus@gmail.com>
2016-02-16 17:27:25 +0300

    Исправления акций

--- resources/views/dashboard/shares/shares.blade.php
+++ resources/views/dashboard/shares/shares.blade.php
@@ -16,7 +16,7 @@
             <div class="table-responsive">
                 <div id="DataTables_Table_0_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer">
                     <div class="row">
-                        <form class="col-sm-12">
+                        <div class="col-sm-12">
                             <table class="table table-striped m-b-none dataTable no-footer" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
                                 <thead>
                                 <tr role="row">
@@ -41,13 +41,16 @@
                                         <td>{{ $Share->updated_at }}</td>
                                         <td class="pull-right">
                                             <a href="{{URL::route('dashboard.shares.edit',$Share->id)}}" class="btn btn-primary"><span class="fa fa-edit"></span> </a>
+
                                             <form action="{{URL::route('dashboard.shares.destroy',$Share->id)}}" method="post" class="pull-right">
                                                 <input type="hidden" name="_method" value="delete">
                                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                                 <button type="submit" class="btn btn-danger"><span class="fa fa-trash-o"></span></button>
                                             </form>
-                        </td>
-                        </tr>
+
+                                        </td>
+
+                                    </tr>
                         @endforeach
 
                         </tbody>
