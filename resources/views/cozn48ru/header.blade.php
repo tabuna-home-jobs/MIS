1546f2b5eca3cd82441cd91a523b12e7b3be8321
hotaru_tomu <angelforevor@mail.ru>
2016-02-25 11:01:59 +0300

    Сок - отображение страниц категорий для услуг, noindex для остальных сайтов

--- resources/views/cozn48ru/header.blade.php
+++ resources/views/cozn48ru/header.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}" >
+    <meta name="robots" content="noindex"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="/cozn48.ru/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/cozn48.ru/style.css">
