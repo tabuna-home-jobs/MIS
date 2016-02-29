192b78d7e55cf2c5b7d53b911d58f31e91d76408
hotaru_tomu <angelforevor@mail.ru>
2016-02-29 12:39:54 +0300

    Лучики - отзывы - вёрстка, обратка - поправила контактные данные

--- resources/views/luchiki48ru/feedback.blade.php
+++ resources/views/luchiki48ru/feedback.blade.php
@@ -8,7 +8,7 @@
         	<h1 class="blueBg center white feedHead">Мы на карте</h1>
             <div class="row">
             	<div class="col-md-12">
-                	<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2423.2438515896024!2d39.592403!3d52.601363!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x413a14e9eece3c35%3A0xdfbc4fe617d626e0!2z0J7QutGC0Y_QsdGA0YzRgdC60LDRjyDRg9C7LiwgNjEsINCb0LjQv9C10YbQuiwg0JvQuNC_0LXRhtC60LDRjyDQvtCx0LsuLCAzOTgwNTk!5e0!3m2!1sru!2sru!4v1432638437060"  height="450" frameborder="0" style="border:0; width: 100%"></iframe></div>
+                	<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2423.409015326709!2d39.58208250461453!3d52.59837735796068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x413a14c15913a36b%3A0x1e0c7ae23f078194!2z0YPQuy4g0J3QtdC00LXQu9C40L3QsCwgMTXQkCwg0JvQuNC_0LXRhtC6LCDQm9C40L_QtdGG0LrQsNGPINC-0LHQuy4sIDM5ODA1OQ!5e0!3m2!1sru!2sru!4v1450430447138"  height="450" frameborder="0" style="border:0; width: 100%"></iframe></div>
                 </div>
             </div>
         <div class="container pad-b-1">
@@ -43,16 +43,16 @@
                 <div class="col-md-6 col-sm-6 contacts">
                     <div class="col-md-6 col-xs-6 center">
                         <i class="fa fa-map-marker fa-big blueText"></i><br><br><br>
-                        (4742) 227-887, 227-8881<br><br>
-                        octavian48@yandex.ru<br><br>
-                        г.Липецк ул.Октябрьская д.61
+                        (4742) 77-03-97.<br><br>
+                        <!--octavian48@yandex.ru<br><br>-->
+                        г. Липецк, ул. Неделина, 15-а
 
                     </div>
                     <div class="col-md-6 col-xs-6 center">
                         <i class="fa fa-clock-o fa-big blueText"></i><br><br><br>
-                        Понедельник-пятница: 8:00-21:00<br><br>
-                        Суббота: 8:00-18:00<br><br>
-                        Воскресенье: 10:00-16:00
+                        Понедельник-пятница: 8:30-20:30<br><br>
+                        Суббота: 8:30-19:00<br><br>
+                        Воскресенье: 9:30-15:30
                     </div>
                     <!-- <div class="get-in-touch-widget">
                     	<ul class="list-unstyled">
--- resources/views/luchiki48ru/reviews.blade.php
+++ resources/views/luchiki48ru/reviews.blade.php
@@ -4,7 +4,7 @@
 
     <div class="page-content">
         <div class="container">
-            <h1>Отзывы о нас</h1>
+            <h1 class="blueText center">ОТЗЫВЫ О НАС</h1>
             <div class="page-text">
 
                 <style>
@@ -24,35 +24,45 @@
                         </div>
                     @endif
                     @foreach($Reviews as $item)
-                    <div class="single-review col-md-12">
-                        <div class="rev-img col-md-2 col-sm-2"><i class="feature-icon fa fa-commenting"></i></div>
-                        <div class="rev-text col-md-10 col-sm-10">
-                            <h4>{{$item['fio']}}</h4>
-                            <span>{{$item['created_at']}}</span>
+                    <div class="single-review col-md-6">
+                        <div class="rev-img col-md-3 col-sm-3 hidden-xs"><i class="feature-icon fa fa-commenting"></i></div>
+                        <div class="rev-text col-md-9 col-sm-9">
                             <p>{!! str_limit($item['content'],300,'...') !!}
                                 </p>
+                            <div class="text-right rev-name">
+                               <strong>- {{$item['fio']}}</strong>
+                            </div>
                         </div>
                     </div>
                     @endforeach
 
                 </div>
-                <div class="row">
-                    {!! $Reviews->render() !!}
-                </div>
 
+            </div>
+        </div>
+    </div>
+    <div class="reviews-form">
+        <h2 class="blueBg center white feedHead">ОСТАВИТЬ ОТЗЫВ</h2>
+        <div class="container">
+            <div class="row">
+                {!! $Reviews->render() !!}
+            </div>
+            <div class="col-md-6 col-sm-6 hidden-xs">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut enim ipsum. Sed malesuada convallis eros quis accumsan. Aliquam erat volutpat. Nam id bibendum libero. Vestibulum auctor in urna vitae pretium. Nam tempor sit amet quam nec ullamcorper. Ut quam nisi, tristique nec euismod ut, bibendum sit amet ligula. Sed semper, augue nec dignissim elementum, tortor est varius lacus, id ullamcorper lectus arcu vel ante. Aliquam erat volutpat. In magna libero, iaculis ac mollis a, blandit ut risus. Nam metus odio, bibendum vel interdum et, vehicula vel eros. In interdum sit amet dui eget pharetra. Nulla facilisi.
 
+                In eget ante sed arcu viverra mattis sit amet vestibulum est. Nam porttitor bibendum ex vitae scelerisque. Phasellus fermentum ullamcorper justo eu accumsan. Vestibulum in metus non turpis iaculis rhoncus at ut metus. Quisque mattis tortor eget diam cursus, eu laoreet ante consequat. In mollis nisi eget felis varius tempus. Suspendisse gravida tincidunt nulla a blandit. Donec venenatis blandit eros. Sed suscipit neque vitae nibh vehicula semper. Aliquam eu vestibulum mauris, convallis pretium leo. Mauris tristique, leo ut ultricies egestas, dui risus tincidunt risus, sit amet dapibus mauris lectus ac neque. Cras vitae efficitur dolor. Ut ut odio ante.
 
+            </div>
+
+            <div class="col-md-6 col-sm-6">
 
                 <form class="form-horizontal" role="form" action="/reviews" method="post">
                     <fieldset>
 
-                        <!-- Form Name -->
-                        <legend>Напишите свой отзыв</legend>
-
                         <!-- Text input-->
                         <div class="form-group">
-                            <label class="col-md-4 control-label" for="textinput">Имя</label>
-                            <div class="col-md-4">
+
+                            <div class="col-md-12">
                                 <input id="textinput" name="fio" type="text" max="255" placeholder="Имя" class="form-control input-md">
                                 <span class="help-block"></span>
                             </div>
@@ -60,30 +70,24 @@
 
                         <!-- Textarea -->
                         <div class="form-group">
-                            <label class="col-md-4 control-label" for="textarea">Комментарий</label>
-                            <div class="col-md-4">
+
+                            <div class="col-md-12">
                                 <textarea class="form-control" id="textarea" name="content" placeholder="Комментарий"></textarea>
                             </div>
                         </div>
 
                         {{csrf_field()}}
 
-                        <!-- Button -->
+                                <!-- Button -->
                         <div class="form-group">
-                            <label class="col-md-4 control-label" for="singlebutton">Оставить отзыв</label>
-                            <div class="col-md-4">
+
+                            <div class="col-md-12">
                                 <button name="singlebutton" type="submit" class="btn btn-primary">ОТПРАВИТЬ</button>
                             </div>
                         </div>
 
                     </fieldset>
                 </form>
-
-
-
-
-
-
             </div>
         </div>
     </div>
