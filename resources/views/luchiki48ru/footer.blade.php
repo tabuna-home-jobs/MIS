22c5904b6f6d57c0f218b5b905ee17340f5e7fbc
hotaru_tomu <angelforevor@mail.ru>
2016-02-29 11:18:05 +0300

    Заменила контакты в шапке

--- resources/views/luchiki48ru/footer.blade.php
+++ resources/views/luchiki48ru/footer.blade.php
@@ -5,7 +5,7 @@
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
 
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2" aria-expanded="false">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
@@ -14,7 +14,7 @@
 
                 </div>
                 <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
                     <ul class="nav navbar-nav">
                         {!! Menu::getLi(7,'topmain','') !!}
                     </ul>
