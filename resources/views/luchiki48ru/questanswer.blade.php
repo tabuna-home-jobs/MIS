20d84d9d32ba4b582358b5db029d54c729ff7863
hotaru_tomu <angelforevor@mail.ru>
2016-03-01 10:33:11 +0300

    Лучки - вопрос-ответ

--- resources/views/luchiki48ru/questanswer.blade.php
+++ resources/views/luchiki48ru/questanswer.blade.php
@@ -2,108 +2,16 @@
 
 @section('content')
 
-
-
-    <!--<section data-stellar-background-ratio="0.3" class="sub-page-banner sub-page-banner-med text-center">
-        <div class="overlay"></div>
-        <div class="container">
-
-            <p>Задайте любой интересующий вас вопрос и наш специалист найдёт ответ</p>
-        </div>
-    </section>-->
-
-
-
     <div class="sub-page-content">
 
         <h1 class="blueText center">Вопросы и ответы</h1>
 
-        <div class="container padding-top-35">
-            <div class="row">
-                <div class="col-md-6">
-                    <h2 class="light bordered">Напиши и узнаешь</h2>
-
-                    <div class="appointment-form clearfix">
-                        <form action="/questanswer" method="post">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="ФИО" required name="fio">
-                            </div>
-                            <div class="form-group">
-                                <input type="email" class="form-control" name="email" required placeholder="Email">
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="phone" required placeholder="Телефон">
-                            </div>
-                            <div class="form-group">
-                                <textarea name="questions" class="form-control" placeholder="Ваш вопрос"></textarea>
-                            </div>
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <input type="submit" class="btn btn-primary" value="Отправить">
-                        </form>
-                    </div>
-
-
-
-
-                </div>
-                <div class="col-md-6">
-                    <h2 class="light bordered">Уверенность и  качество</h2>
-                    <div class="feature">
-                        <i class="pull-left feature-icon fa fa-user-md"></i>
-                        <div class="feature-content">
-                            <h5><a href="/team">Наши специалисты</a></h5>
-                            <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining popularised only five Power of centuries.</p>
-                            <a href="/team"><dfn>- Перейти</dfn></a>
-                        </div>
-                    </div>
-
-                    <div class="feature">
-                        <i class="pull-left feature-icon fa fa-phone-square"></i>
-                        <div class="feature-content">
-                            <h5><a href="/feedback">Записаться на приём</a></h5>
-                            <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining popularised only five Power of centuries.</p>
-                            <a href="/feedback"><dfn>-  Перейти</dfn></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
 
         <div class="padding-top-35 after-booking-sec text-center">
-            <!--
-            <div class="row">
-
-                @foreach($QuestAnswers as $QA)
-                <div class="col-md-12 blog-wrapper clearfix  padding-top-35">
-
-                    <h2 class="light bordered main-title">Вопрос задал : <span class="pull-right">{{$QA->fio}}</span></h2>
-
-                    <div class="col-md-12">
-                        <div class="blog-content">
-                            <p class="text-justify">{{$QA->questions}}</p>
-                            <hr><h4 class="blog-title text-center">Ответ специалиста:</h4><hr>
-                            <p class="text-justify">{{$QA->answer}}</p>
-
-                            <small class="pull-right">{{$QA->created_at}}</small>
-                        </div>
-                    </div>
-
-
-                </div>
-                @endforeach
-
-
-
-                    {!! $QuestAnswers->render() !!}
-
-            </div>
-            -->
-
             <ul class="medicom-feature-list list-inline text-left" id="accordion" role="tablist"
                 aria-multiselectable="true">
 
-                @foreach($QuestAnswers as $QA)
+                @foreach($QuestAnswers as  $key =>$QA)
                     <li>
 
                         <div class="container panel panel-default">
@@ -119,10 +27,10 @@
 
 
                                     <p class="text-right">
-                                        <strong class=" text-right">
+                                        <div class="text-right black">
                                             <!--{{$QA->getCategory->name or ''}}-->
                                             <br>- {{$QA->fio}}
-                                        </strong>
+                                        </div>
                                     </p>
 
                                 </h4>
@@ -130,15 +38,15 @@
 
                             </div>
 
-                            <div id="collapse-answer-{{$QA->id}}" class="panel-collapse collapse" role="tabpanel"
+                            <div id="collapse-answer-{{$QA->id}}" class="panel-collapse" role="tabpanel"
                                  aria-labelledby="heading-answer-{{$QA->id}}">
                                 <div class="panel-body blog-content">
                                     {{$QA->answer}}
-                                    <p class="text-right">
-                                        <small>С уважением {{$QA->getDoctor->fio or ''}}</small>
-                                        <small><br>
+                                    <p class="text-right black">
+                                        - {{$QA->getDoctor->fio or ''}}
+                                        <!--<small><br>
                                             <date>{{$QA->updated_at->diffForHumans()}}</date>
-                                        </small>
+                                        </small>-->
                                     </p>
                                 </div>
                             </div>
@@ -146,6 +54,65 @@
 
                         </div>
                     </li>
+                    @if( ($key+1) == floor( count($QuestAnswers)/2 ) )
+                        <div class="quest-form white">
+                            <h2 class="blueBg center  feedHead">Напиши и узнаешь</h2>
+                            <div class="container">
+                                <div class="col-md-6">
+                                    <p>
+
+
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae urna eu ipsum dictum faucibus. Mauris placerat scelerisque purus id luctus. Nam sit amet arcu laoreet, pulvinar libero ac, facilisis sapien. Proin erat dolor, faucibus sit amet sagittis et, sagittis sed nulla. Fusce vel risus mauris. Nulla quis orci ornare, porta lorem id, lobortis dolor. Mauris ligula leo, iaculis ac velit ullamcorper, blandit euismod erat. Sed dictum purus sed viverra mattis. Sed ultricies odio id consectetur ultrices. Suspendisse venenatis orci nec nibh pellentesque, et malesuada lacus hendrerit. Sed et sem ante. Nullam et nunc ac massa volutpat ornare. Praesent finibus elit sem, in tristique est vestibulum id. Vivamus lobortis scelerisque turpis, non vulputate velit dignissim nec. Pellentesque varius arcu commodo rhoncus cursus. Praesent sagittis posuere odio eu sodales.
+
+                                        Donec nec rutrum orci, ac rutrum purus. Praesent finibus malesuada ultrices. Donec imperdiet vel sem in ornare. Suspendisse gravida fringilla nisl lacinia tempor. Cras rutrum a arcu vel posuere. Nullam ex purus, faucibus sed erat nec, consequat ornare est. Sed scelerisque at purus eget faucibus. Proin vitae sapien lectus. Integer tincidunt, augue in pretium scelerisque, massa neque luctus arcu, at fringilla augue mauris a leo.
+                                    </p>
+                                    <!--<h2 class="light bordered">Уверенность и  качество</h2>
+                                    <div class="feature">
+                                        <i class="pull-left feature-icon fa fa-user-md"></i>
+                                        <div class="feature-content">
+                                            <h5><a href="/team">Наши специалисты</a></h5>
+                                            <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining popularised only five Power of centuries.</p>
+                                            <a href="/team"><dfn>- Перейти</dfn></a>
+                                        </div>
+                                    </div>
+
+                                    <div class="feature">
+                                        <i class="pull-left feature-icon fa fa-phone-square"></i>
+                                        <div class="feature-content">
+                                            <h5><a href="/feedback">Записаться на приём</a></h5>
+                                            <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining popularised only five Power of centuries.</p>
+                                            <a href="/feedback"><dfn>-  Перейти</dfn></a>
+                                        </div>
+                                    </div>-->
+                                </div>
+
+                                <div class="col-md-6">
+
+
+                                    <div class="appointment-form clearfix">
+                                        <form action="/questanswer" method="post">
+                                            <div class="form-group">
+                                                <input type="text" class="form-control" placeholder="ФИО" required name="fio">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="email" class="form-control" name="email" required placeholder="Email">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="text" class="form-control" name="phone" required placeholder="Телефон">
+                                            </div>
+                                            <div class="form-group">
+                                                <textarea name="questions" class="form-control" placeholder="Ваш вопрос"></textarea>
+                                            </div>
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                            <input type="submit" class="btn btn-primary" value="Отправить">
+                                        </form>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    @endif
                 @endforeach
 
 
