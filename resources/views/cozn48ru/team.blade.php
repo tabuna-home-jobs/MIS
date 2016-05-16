8d01ca62811cd5e247abc63146845f0d2194f181
hotaru_tomu <angelforevor@mail.ru>
2016-05-16 16:11:12 +0300

    Остеопатия - убрали соцсети у спецов

--- resources/views/cozn48ru/team.blade.php
+++ resources/views/cozn48ru/team.blade.php
@@ -19,13 +19,14 @@
             	<div class="col-md-4 padding-bottom-60 clearfix wrapper-team-elem">
                     <div class="doctors-img text-center">
                         <img src="{{$spec->avatar}}" class="img-circle" width="234" alt="{{$spec->fio}}" title="{{$spec->fio}}">
-
+                        {{--
                         <ul class="list-unstyled social2">
                             <li><a href="#." class="fb"><i class="fa fa-twitter"></i></a></li>
                             <li><a href="#." class="twitter"><i class="fa fa-facebook"></i></a></li>
                             <li><a href="#." class="twitter"><i class="fa fa-vk"></i></a></li>
                             <li><a href="#." class="odnok"><i class="fa fa-circle-o"></i></a></li>
                         </ul>
+                        --}}
                         </div>
                     <div class="doctors-detail text-center">
                         <a href="/team/{{$spec['id']}}"><h4>{{$spec->fio}}</h4></a>
