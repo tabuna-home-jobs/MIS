8d00cdf4c743e4a9c75d90d40701ba611da9ade9
hotaru_tomu <angelforevor@mail.ru>
2016-07-28 12:09:58 +0300

    ЗН - описание для преимкществ

--- resources/views/newzdorovie48ru/_blocks/index_dignity.blade.php
+++ resources/views/newzdorovie48ru/_blocks/index_dignity.blade.php
@@ -5,7 +5,8 @@
             <h1 class="text-center  m-b-xxl">Наши достоинства</h1>
 
             @foreach($block->items as $key => $item)
-                <div class="col-sm-3 col-xs-6 @if($key > 1) hidden-xs @endif">
+                <div class="col-sm-3 col-xs-6 @if($key > 1) hidden-xs @endif
+                @if($item->text != "") item-dost @endif">
                     <p class="h3 m-b-xl inline b b-white rounded wrapper-xl">
                         <i class="fa w-1x fa {{ $item->descript }} fa-2x" aria-hidden="true"></i>
                     </p>
@@ -13,6 +14,14 @@
                     <div class="m-b-xl">
                         <p class="h4 m-t-none l-h-1x">{{ $item->name }}</p>
                     </div>
+
+
+                    @if($item->text != "")
+                    <div class="hover-dospt">
+
+                        {!! $item->text !!}
+                    </div>
+                        @endif
                 </div>
             @endforeach
         </div>
