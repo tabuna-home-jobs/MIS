cdcba3c3dcfbc682b986d6a6e120253ff6d69dac
hotaru_tomu <angelforevor@mail.ru>
2016-08-04 11:53:08 +0300

    Лучики -правки не валидного html

--- resources/views/sokzn48ru/_layout/footMenu.blade.php
+++ resources/views/sokzn48ru/_layout/footMenu.blade.php
@@ -1,6 +1,6 @@
 <ul id="footer-menu" class="footer-menu">
     @foreach($items as $item)
-        <li id="menu-item-1009" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-1009 {{$item['class']}}">
+        <li  class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-1009 {{$item['class']}}">
             <a href="{{$item['link']}}">{{$item['label']}}</a>
         </li>
     @endforeach
