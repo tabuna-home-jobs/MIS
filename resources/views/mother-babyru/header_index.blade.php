5b1fbfc7ad7e67f0658c7a18d7e781d35232c85f
hotaru_tomu <angelforevor@mail.ru>
2016-09-14 12:22:05 +0300

    Мать и дитя - вёрстка - подключение библиотек

--- resources/views/mother-babyru/header_index.blade.php
+++ resources/views/mother-babyru/header_index.blade.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="/mother-baby.ru/css/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="/mother-baby.ru/css/slick.css" type="text/css">
     <link rel="stylesheet" href="/mother-baby.ru/style.css" type="text/css">
+    <script src="/mother-baby.ru/js/jquery.min.js"></script>
     <script src="/mother-baby.ru/js/bootstrap.min.js"></script>
     <script src="/mother-baby.ru/js/slick.min.js"></script>
     <script src="/mother-baby.ru/js/app.js"></script>
@@ -35,6 +36,62 @@
 <body>
 
 <header class="index-head">
+
+
+
+
+                    <nav class="navbar navbar-top  container-fluid">
+                        <div class="container">
+                            <div class="row">
+                                <!-- Brand and toggle get grouped for better mobile display -->
+                                <div class="navbar-header">
+                                    <button type="button" class="navbar-toggle collapsed text-black" data-toggle="collapse"
+                                            data-target="#navbar-collapse-1" aria-expanded="false">
+                                        <span class="sr-only">Навигация</span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                    </button>
+
+                                    <a class="navbar-brand" href="/">
+                                        <img src="/mother-baby.ru/img/logo.png" alt="Здоровье Нации" class="img-responsive">
+                                    </a>
+                                </div>
+
+                                <!-- Collect the nav links, forms, and other content for toggling -->
+                                <div class="collapse navbar-collapse" id="navbar-collapse-1">
+                                    {!!Menu::getZdorovieNacii('2','Верхнее меню','','menu')!!}
+
+
+                                    <ul class="soc nav navbar-nav nav-bar-right">
+                                        <li><a href="/"><img src="/mother-baby.ru/img/vk.png"></a></li>
+                                        <li><a href="/"><img src="/mother-baby.ru/img/fb.png"></a></li>
+                                        <li><a href="/"><img src="/mother-baby.ru/img/odnoklasniki.png"></a></li>
+                                    </ul>
+
+                                    <ul class="nav navbar-nav navbar-left">
+                                        <li><a href="#">Link</a></li>
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                                            <ul class="dropdown-menu">
+                                                <li><a href="#">Action</a></li>
+                                                <li><a href="#">Another action</a></li>
+                                                <li><a href="#">Something else here</a></li>
+                                                <li role="separator" class="divider"></li>
+                                                <li><a href="#">Separated link</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+
+                                </div><!-- /.navbar-collapse -->
+                            </div>
+                        </div><!-- /.container-fluid -->
+                    </nav>
+
+
+
+
+
 
 
 </header>
